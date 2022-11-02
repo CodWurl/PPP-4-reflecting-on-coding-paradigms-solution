66522b4c4cde3e89eb60e742c947ad5585031413
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-11-01 23:04:21 -0400

    save changes

--- functional_solution.py
+++ functional_solution.py
@@ -14,16 +14,3 @@ def flatten_and_sort(array):
 print(flatten_and_sort([1,[55,17,12],3, [22,19,35],9,2]))
    
 
-'''
-Make sure to answer the following questions about your coding process:
-
-How does this solution ensure data immutability?
-
-Is this solution a pure function? Why or why not?
-
-Is this solution a higher order function? Why or why not?
-
-Would it have been easier to solve this problem using a different programming style?
-
-Why in particular is functional programming a helpful paradigm when solving this problem?
-'''
